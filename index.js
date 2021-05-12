918b34653dd9751456faf7b7e910b9104a333309
Michael Schnupp <michael.schnupp@accenture.com>
2021-05-13 00:20:00 +0200

    .

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const express = require('express')
+const app = require('express')()
 const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-const app = express()
 const port = process.env.PORT || 4000
 
 const logs = []
 
-app.use(bodyParser.json())
-//app.use(bodyParser.text({type: '*/*'}));
-
 app.get('/', (req, res) => {
-  console.log("get")
-  console.log(req.headers)
   res.write("current log:\n")
   res.write('========\n')
   logs.forEach(([type,name,time])=> res.write(`${type}\t${time}\t${name}\n`))
@@ -20,38 +15,21 @@ app.get('/', (req, res) => {
 })
 
 app.post('/notification', (req, res) => {
-  if (!req.is('application/json')) {
-    console.log("not json")
+  if (req.is('application/json') && req.headers['authorization'] == "CNHwZ_Z6RKqj9ymYwmp0Og" ) {
+    switch (req.body.event) {
+      case "meeting.participant_joined":
+        logs.push(["join", req.body.payload.object.participant.user_name, req.body.payload.object.participant.join_time])
+      case "meeting.participant_left":
+        logs.push(["left", req.body.payload.object.participant.user_name, req.body.payload.object.participant.leave_time])
+      default:
+        console.log("unexpected type: " + req.body.event)
+    }
+    res.sendStatus(204)
+  } else {
+    console.log("wrong request")
     console.log(req.headers)
     res.send(400)
-  } else if (!req.headers['authorization'] == "CNHwZ_Z6RKqj9ymYwmp0Og") {
-    console.log("not authorized")
-    console.log(req.headers)
-    res.send(400);
-  } else {
-    console.log("all is well")
-    console.log(req.headers)
-    console.log(req.body)
-    if(req.body.event == "meeting.participant_joined") {
-      const participant = req.body.payload.object.participant
-      console.log("======== join =========")
-      console.log(participant.user_name)
-      console.log(participant.join_time)
-      console.log("+++++++++++++++++++++++")
-      logs.push(["join", participant.user_name, participant.join_time])
-    } else if(req.body.event == "meeting.participant_left") {
-      const participant = req.body.payload.object.participant
-      console.log("======== leave ========")
-      console.log(participant.user_name)
-      console.log(participant.leave_time)
-      console.log("+++++++++++++++++++++++")
-      logs.push(["left", participant.user_name, participant.leave_time])
-    } else {
-      console.log("unexpected type: " + req.body.event)
-    }
   }
-  res.send('Chat received')
 })
-
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
