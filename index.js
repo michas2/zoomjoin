8a2e6b0cf45e7c3c0b2ca4815a4a8866489bfd23
Michael Schnupp <michael.schnupp@accenture.com>
2021-05-12 21:59:13 +0200

    .

--- index.js
+++ index.js
@@ -4,18 +4,35 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
-//app.use(bodyParser.json())
-app.use(bodyParser.text({type: '*/*'}));
+app.use(bodyParser.json())
+//app.use(bodyParser.text({type: '*/*'}));
 
 app.get('/', (req, res) => {
   console.log("get")
   console.log(req.headers)
-  res.send('Welcome!')
+  res.send('Welcome!!')
 })
 
 app.post('/notification', (req, res) => {
-  console.log(req.headers)
-  console.log(req.body)
+  if (!req.is('application/json')) {
+    console.log("not json");
+    console.log(req.headers)
+    res.send(400);
+  } else if (!req.headers['authorization'] == "CNHwZ_Z6RKqj9ymYwmp0Og") {
+    console.log("not authorized");
+    console.log(req.headers)
+    res.send(400);
+  } else {
+    console.log("all is well");
+    console.log(req.headers)
+    console.log(req.body)
+    console.log("=======================")
+    console.log(req.body.event)
+    console.log(req.body.payload.object.participant.user_name)
+    console.log(req.body.payload.object.participant.join_time)
+    console.log(req.body.payload.object.participant.leave_time)
+    console.log("+++++++++++++++++++++++")
+  }
   res.send('Chat received')
 })
 
