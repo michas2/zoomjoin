f87bc194674725d6be1c8086ee40b7c42f3ae666
Michael Schnupp <michael.schnupp@accenture.com>
2021-05-12 21:31:43 +0200

    .

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+app.use(bodyParser.text({type: '*/*'}));
 
 app.get('/', (req, res) => {
+  console.log("get")
+  console.log(req.headers)
   res.send('Welcome!')
 })
 
-app.post('/receive', (req, res) => {
+app.post('/notification', (req, res) => {
+  console.log(req.headers)
   console.log(req.body)
   res.send('Chat received')
 })
