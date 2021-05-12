e3e11d6712517074830be81c3d0465882cff3f88
Michael Schnupp <michael.schnupp@accenture.com>
2021-05-12 23:11:16 +0200

    .

--- index.js
+++ index.js
@@ -4,34 +4,51 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
+const log = []
+
 app.use(bodyParser.json())
 //app.use(bodyParser.text({type: '*/*'}));
 
 app.get('/', (req, res) => {
   console.log("get")
   console.log(req.headers)
-  res.send('Welcome!!')
+  res.write("current log:\n")
+  res.write('========')
+  logs.forEach(([type,name,time])=> res.write(util.format("%6s %s %s\n",type,time,name)))
+  res.write('========')
+  res.end
 })
 
 app.post('/notification', (req, res) => {
   if (!req.is('application/json')) {
-    console.log("not json");
+    console.log("not json")
     console.log(req.headers)
-    res.send(400);
+    res.send(400)
   } else if (!req.headers['authorization'] == "CNHwZ_Z6RKqj9ymYwmp0Og") {
-    console.log("not authorized");
+    console.log("not authorized")
     console.log(req.headers)
     res.send(400);
   } else {
-    console.log("all is well");
+    console.log("all is well")
     console.log(req.headers)
     console.log(req.body)
-    console.log("=======================")
-    console.log(req.body.event)
-    console.log(req.body.payload.object.participant.user_name)
-    console.log(req.body.payload.object.participant.join_time)
-    console.log(req.body.payload.object.participant.leave_time)
-    console.log("+++++++++++++++++++++++")
+    if(req.body.event == "meeting.participant_joined") {
+      const participant = req.body.payload.object.participant
+      console.log("======== join =========")
+      console.log(participant.user_name)
+      console.log(participant.join_time)
+      console.log("+++++++++++++++++++++++")
+      log.push(["join", participant.user_name, participant.join_time])
+    } else if(req.body.event == "meeting.participant_left") {
+      const participant = req.body.payload.object.participant
+      console.log("======== leave ========")
+      console.log(participant.user_name)
+      console.log(participant.leave_time)
+      console.log("+++++++++++++++++++++++")
+      log.push(["left", participant.user_name, participant.leave_time])
+    } else {
+      console.log("unexpected type: " + req.body.event)
+    }
   }
   res.send('Chat received')
 })
